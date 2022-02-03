3bab7d0b0e804f313aa9978266dfc5ce474eeec7
Brian Clements <brian.l.clements03@gmail.com>
2022-02-03 11:43:14 -0600

    my acquire.py is fully ready, minus code commenting

--- acquire.py
+++ acquire.py
@@ -22,9 +22,10 @@ def get_link(article):
 
 
 
-def get_blog_articles():
+def get_blog_articles(cached=False):
     filename = 'codeup_blog_articles.json'
-    if os.path.isfile(filename):
+    if cached==True:
+#         os.path.isfile(filename):
         return pd.read_json(filename)
     
     else:
@@ -33,14 +34,17 @@ def get_blog_articles():
         soup = BeautifulSoup(response.text)
         articles = soup.find_all('h2', class_ = 'entry-title')
         for article in articles:
-            title = article.text
             link = get_link(article)
             article_response = requests.get(link, headers={'user-agent': 'Codeup DS Hopper'})
             article_soup = BeautifulSoup(article_response.text)
-            article_content = [p.text for p in article_soup.find_all('p')]
+            title = article_soup.find('h1',class_='entry-title').text
+            date_published = article_soup.find('span',class_='published').text
+            article_content = article_soup.find('div',class_='entry-content').text.strip()
 
             article = {
-                'title': title, 'article_content': article_content
+                'title': title, 
+                'date_published': date_published,
+                'article_content': article_content
             }
             article_list.append(article)
         df = pd.DataFrame(article_list)
@@ -48,15 +52,18 @@ def get_blog_articles():
     return df
 
 
+
+
 # Great, this function is working now
 
 # let's not forget to comment the code
 
 
-def get_news_articles():
+def get_news_articles(cached=False):
     filename = 'inshort_news_articles.json'
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
+    if cached == True:
+#     os.path.isfile(filename):
+        return pd.read_json(filename)
     else:
         base_url = 'https://inshorts.com/en/read/'
         sections = ["business","sports","technology","entertainment"]
@@ -69,10 +76,12 @@ def get_news_articles():
                 title = card.find('span',itemprop='headline').text
                 author = card.find('span', class_="author").text
                 content = card.find('div', itemprop="articleBody").text
+                date_published = card.find('span', clas="date").text
                 article = ({'section': section, 
                             'title': title, 
                             'author': author, 
-                            'content': content})
+                            'content': content,
+                            'date_published': date_published})
                 articles.append(article)
         df = pd.DataFrame(articles)
         df.to_json('inshort_news_articles.json')

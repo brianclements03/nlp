c17eac340a84e6433e3bff59a1203f1d367dc454
Brian Clements <brian.l.clements03@gmail.com>
2022-02-02 18:19:12 -0600

    add acquire.py and create get_links function--attempted to create a get_blog_articles function with caching but this is not currently up and running...

---
+++ acquire.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+
+def get_links():
+    link_list = []
+    response = requests.get('https://codeup.com/blog/', headers={'user-agent': 'Codeup DS Hopper'})
+    soup = BeautifulSoup(response.text)
+    articles = soup.find_all('h2', class_ = 'entry-title')
+    for article in articles:
+        link = article.a.attrs['href']
+        link_list.append(link)
+    return link_list
+
+### the following function is still not proven...taking too long to run at the coffee shop
+def get_blog_articles():
+    filename = 'codeup_blog_articles.json'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    
+    else:
+        article_list=[]
+        response = requests.get('https://codeup.com/blog/', headers={'user-agent': 'Codeup DS Hopper'})
+        soup = BeautifulSoup(response.text)
+        articles = soup.find_all('h2', class_ = 'entry-title')
+        for article in articles:
+#             response = requests.get('https://codeup.com/blog/', headers={'user-agent': 'Codeup DS Hopper'})
+#             soup = BeautifulSoup(response.text)
+#             articles = soup.find_all('h2', class_ = 'entry-title')
+            title = article.text
+            links = get_links()
+            for link in links:
+                article_response = requests.get(link, headers={'user-agent': 'Codeup DS Hopper'})
+                article_soup = BeautifulSoup(article_response.text)
+                article_content = [p.text for p in article_soup.find_all('p')]
+
+                article = {
+                    'title': title, 'article_content': article_content
+                }
+            article_list.append(article)
+        df = pd.DataFrame(article_list)
+        df.to_json('codeup_blog_articles.json')
+    return df
+

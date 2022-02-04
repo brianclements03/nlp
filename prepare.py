b0ca5092861a25852fdff8adf819670ff80249e7
Brian Clements <brian.l.clements03@gmail.com>
2022-02-04 14:17:47 -0600

    did troubleshooting with adam re: the prepare functions, ended up just using the ones from the exercise review for time. add pdf doc on the vader sentiment analyzer

--- prepare.py
+++ prepare.py
@@ -8,38 +8,119 @@ import pandas as pd
 from time import strftime
 import acquire
 
-# I'M TAKING A NEW APPROACH
-# def basic_clean(df):
-#     for col in df.columns:
-#         df[col] = df[col].str.lower().str.normalize('NFKD')\
-#         .str.encode('ascii', 'ignore')\
-#         .str.decode('utf-8', 'ignore')\
-#         .replace(r"[^a-z0-9'\s]", '',regex=True)
-#     return df
+# DUE TO MANY ISSUES WITH MY PREP FUNCTIONS, FOR NOW I WILL USE THOSE PRODUCED BY ADAM IN CLASS
 
-def basic_clean(df):
-    df = original.str.lower().replace(r'[^\w\s]', '', regex=True)\
-    .str.normalize('NFKD').str.encode('ascii', 'ignore').str.decode('utf-8', 'ignore')
-    return df
+def basic_clean(string):
+    '''
+    This function takes in a string and
+    returns the string normalized.
+    '''
+    string = unicodedata.normalize('NFKD', string)\
+             .encode('ascii', 'ignore')\
+             .decode('utf-8', 'ignore')
+    string = re.sub(r'[^\w\s]', '', string).lower()
+    return string
 
-# NEW APPROACH
-# def tokenize(df):
-#     for col in df.columns:
-#         df[col] = df[col].apply(tokenizer.tokenize)
-#     return df
 
-def stem(df,col):
+def tokenize(string):
+    '''
+    This function takes in a string and
+    returns a tokenized string.
+    '''
+    # Create tokenizer.
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    
+    # Use tokenizer
+    string = tokenizer.tokenize(string, return_str = True)
+    
+    return string
+
+
+def stem(string):
+    '''
+    This function takes in a string and
+    returns a string with words stemmed.
+    '''
+    # Create porter stemmer.
     ps = nltk.porter.PorterStemmer()
-    stems = df[col].apply(lambda row: [ps.stem(word) for word in row])
-    df['stemmed'] = stems.str.join(' ')
-    return df
+    
+    # Use the stemmer to stem each word in the list of words we created by using split.
+    stems = [ps.stem(word) for word in string.split()]
+    
+    # Join our lists of words into a string again and assign to a variable.
+    string = ' '.join(stems)
+    
+    return string
 
 
-def remove_stopwords(df,col,extra_words=[],exclude_words=['no','not']):
+def lemmatize(string):
+    '''
+    This function takes in string for and
+    returns a string with words lemmatized.
+    '''
+    # Create the lemmatizer.
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # Use the lemmatizer on each word in the list of words we created by using split.
+    lemmas = [wnl.lemmatize(word) for word in string.split()]
+    
+    # Join our list of words into a string again and assign to a variable.
+    string = ' '.join(lemmas)
+    
+    return string
+
+
+def remove_stopwords(string, extra_words = [], exclude_words = []):
+    '''
+    This function takes in a string, optional extra_words and exclude_words parameters
+    with default empty lists and returns a string.
+    '''
+    # Create stopword_list.
     stopword_list = stopwords.words('english')
-    stopword_list.extend(extra_words)
+    
+    # Remove 'exclude_words' from stopword_list to keep these in my text.
     stopword_list = set(stopword_list) - set(exclude_words)
-    words = df[col].str.split()
-    filtered_words = words.apply(lambda row: [word for word in row if word not in stopword_list])
-    df['stopwords_removed' + col] = filtered_words.str.join(' ')
-    return df
+    
+    # Add in 'extra_words' to stopword_list.
+    stopword_list = stopword_list.union(set(extra_words))
+
+    # Split words in string.
+    words = string.split()
+    
+    # Create a list of words from my string with stopwords removed and assign to variable.
+    filtered_words = [word for word in words if word not in stopword_list]
+    
+    # Join words in the list back into strings and assign to a variable.
+    string_without_stopwords = ' '.join(filtered_words)
+    
+    return string_without_stopwords
+
+
+def prep_article_data(df, column, extra_words=[], exclude_words=[]):
+    '''
+    This function take in a df and the string name for a text column with 
+    option to pass lists for extra_words and exclude_words and
+    returns a df with the text article title, original text, stemmed text,
+    lemmatized text, cleaned, tokenized, & lemmatized text with stopwords removed.
+    '''
+    df['clean'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    df['stemmed'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(stem)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    df['lemmatized'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(lemmatize)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    return df[['title', column,'clean', 'stemmed', 'lemmatized']]

d23ee467733a69d56103c8a4e758032b2ab6b8a6
Brian Clements <brian.l.clements03@gmail.com>
2022-02-03 22:39:52 -0600

    complete more exercises

--- prepare.py
+++ prepare.py
@@ -8,17 +8,28 @@ import pandas as pd
 from time import strftime
 import acquire
 
+# I'M TAKING A NEW APPROACH
+# def basic_clean(df):
+#     for col in df.columns:
+#         df[col] = df[col].str.lower().str.normalize('NFKD')\
+#         .str.encode('ascii', 'ignore')\
+#         .str.decode('utf-8', 'ignore')\
+#         .replace(r"[^a-z0-9'\s]", '',regex=True)
+#     return df
 
 def basic_clean(df):
-    for col in df.columns:
-        df[col] = df[col].str.lower().str.normalize('NFKD')\
-        .str.encode('ascii', 'ignore')\
-        .str.decode('utf-8', 'ignore')\
-        .replace(r"[^a-z0-9'\s]", '',regex=True)
+    df = original.str.lower().replace(r'[^\w\s]', '', regex=True)\
+    .str.normalize('NFKD').str.encode('ascii', 'ignore').str.decode('utf-8', 'ignore')
     return df
 
+# NEW APPROACH
+# def tokenize(df):
+#     for col in df.columns:
+#         df[col] = df[col].apply(tokenizer.tokenize)
+#     return df
 
-def tokenize(df):
-    for col in df.columns:
-        df[col] = df[col].apply(tokenizer.tokenize)
+def stem(df,col):
+    ps = nltk.porter.PorterStemmer()
+    stems = df[col].apply(lambda row: [ps.stem(word) for word in row])
+    df['stemmed'] = stems.str.join(' ')
     return df
